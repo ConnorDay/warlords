7c028a8200067a88762bb8cf7865917d12554861
Connor Day <connor.t.day@gmail.com>
2023-02-26 18:26:53 -0500

    update migration script

--- module/helpers/migration.mjs
+++ module/helpers/migration.mjs
@@ -5,25 +5,36 @@ export async function migrate() {
 
 async function migrateItems() {
     for (let item of game.items) {
-        const system = {...item.system};
+        const system = { ...item.system };
         //No Migration for items yet
 
-        await item.update( {system: system} )
+        if (system.rolls && system.rolls.attack !== undefined) {
+            const attack = system.rolls.attack;
+            const damage = system.rolls.damage;
+            system.rolls = null;
+            await item.update({ system: system });
+            system.rolls = {
+                0: { name: "Attack Roll", formula: attack },
+                1: { name: "Damage", formula: damage },
+            };
+        }
+
+        await item.update({ system: system });
     }
 }
 
 async function migrateActors() {
-    for (let actor of game.actors){
-        const system = {...actor.system}
+    for (let actor of game.actors) {
+        const system = { ...actor.system };
 
         //Update attack_bonus
-        if (system.attack_bonus !== undefined || system.attack_bonus !== null){
+        if (system.attack_bonus !== undefined || system.attack_bonus !== null) {
             system.attributes.attack_bonus = {
                 value: system.attack_bonus,
                 bonus: 0,
-            }
+            };
         }
 
-        await actor.update( {system: system})
+        await actor.update({ system: system });
     }
 }
