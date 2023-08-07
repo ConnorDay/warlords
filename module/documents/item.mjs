97f197722342f99aec84319231277a16d97475cc
Connor Day <connor.t.day@gmail.com>
2023-08-07 15:54:50 -0400

    Minor overhaul of roll system

--- module/documents/item.mjs
+++ module/documents/item.mjs
@@ -1,3 +1,4 @@
+import { WarlordsRoll } from "./roll.mjs";
 /**
  * Extend the basic Item with some very simple modifications.
  * @extends {Item}
@@ -59,20 +60,25 @@ export class WarlordsItem extends Item {
 
             // Invoke the roll and submit it to chat.
             let content = "";
+            const rolls = [];
             for (let i in rollData.item.rolls) {
                 const roll = rollData.item.rolls[i];
-                const r = new Roll(roll.formula, rollData);
+                const data = {
+                    ...rollData,
+                    name: roll.name,
+                };
+                const r = new WarlordsRoll(roll.formula, data);
                 await r.evaluate({ async: true });
-                content += `<p><b>${roll.name}: </b>${r.result} = ${r.total}</p>`;
+                content += await r.render();
+                rolls.push(r);
             }
-
-            // If you need to store the value first, uncomment the next line.
-            // let result = await roll.roll({async: true});
             ChatMessage.create({
                 speaker: speaker,
                 rollMode: rollMode,
                 flavor: label,
+                sound: CONFIG.sounds.dice,
                 content: content,
+                rolls: rolls,
             });
         }
     }
--- module/helpers/templates.mjs
+++ module/helpers/templates.mjs
@@ -13,5 +13,6 @@ export const preloadHandlebarsTemplates = async function () {
         "systems/warlords/templates/actor/parts/actor-resources.html",
         "systems/warlords/templates/actor/parts/actor-skills.html",
         "systems/warlords/templates/item/parts/item-rolls.html",
+        "systems/warlords/templates/roll/chat-message.html",
     ]);
 };
